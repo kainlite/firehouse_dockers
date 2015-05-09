3ce396fa99c3f4142d9127c4f755ab0d27df3a16
Néstor Coppi <nestorcoppi@gmail.com>
2015-05-09 01:23:20 -0300

    Server fixes

---
+++ firehouse/starting_the_milonga.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -U postgres \
+    -c "CREATE ROLE firehouse WITH SUPERUSER \
+     CREATEDB CREATEROLE LOGIN ENCRYPTED \
+     PASSWORD 'firehouse' NOINHERIT VALID UNTIL 'infinity';"
+
+export RAILS_ENV=production
+cd /firehouse
+bundle install --without development test --deployment --quiet
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake assets:precompile
+bundle exec rake db:seed
+
+mkdir -p uploads
+mkdir -p tmp/uploads
