b66c82a1da3ed6e5c605eed2276aa8a8ea140141
Néstor Coppi <nestorcoppi@gmail.com>
2015-05-10 15:53:42 -0300

    Lala

--- firehouse/starting_the_milonga.sh
+++ firehouse/starting_the_milonga.sh
@@ -9,8 +9,5 @@ export RAILS_ENV=production
 cd /firehouse
 bundle exec rake db:create
 bundle exec rake db:migrate
-bundle exec rake assets:precompile
+bundle exec rake assets:clean assets:precompile
 bundle exec rake db:seed
-
-mkdir -p uploads
-mkdir -p tmp/uploads
