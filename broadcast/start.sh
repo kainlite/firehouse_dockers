e3002fbcca712f2c0443de47faece05f5f55e585
Néstor Coppi <nestorcoppi@gmail.com>
2015-05-30 01:23:03 -0300

    Broadcast start

---
+++ broadcast/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "entrose"
+echo $(ps aux |grep pulse)
+pulseaudio -k
+pulseaudio -D
+sleep 2
+echo $(ps aux |grep pulse)
+echo $(ps aux |grep icecast)
+/etc/init.d/icecast2 start
+sleep 2
+echo $(ps aux |grep icecast)
+darkice
