13ede04d728ddf0eb109aa13a58521f509ebb079
Néstor Coppi <nestorcoppi@gmail.com>
2015-08-01 13:40:26 -0300

    Broad

--- broadcast/start.sh
+++ broadcast/start.sh
@@ -3,7 +3,8 @@
 echo "entrose"
 echo $(ps aux |grep pulse)
 pulseaudio -k
-pulseaudio -D
+sleep 1
+pulseaudio --system -D
 sleep 2
 echo $(ps aux |grep pulse)
 echo $(ps aux |grep icecast)
@@ -12,7 +13,19 @@ sleep 2
 echo $(ps aux |grep icecast)
 
 cd /firehouse_audio && rake&
+sleep 1
 echo $(ps aux |grep rake)
 
 cd /
+
+#cd /
+
+#echo $(which linphonecsh)
+#echo $(whereis linphonecsh)
+#linphonecsh init
+#echo $(ps aux |grep linphone)
+#sleep 2
+#linphonecsh register --host 192.168.1.10 --username linphone --password linphone1234 && \
+  #linphonecsh generic 'autoanswer enable'
+
 darkice
